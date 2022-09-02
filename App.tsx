43e9db7c42b93ccff1fed6445e121211189c951b
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-09-02 15:26:16 -0300

    ✨feat: Add and configuring firebase

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import * as Font from "expo-font";
 import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 import { ThemeProvider } from "styled-components";
-import theme from "@src/theme";
+import theme from "./src/theme";
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
