458ef12a803459ed5648a585792e132d0ae6a969
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-09-12 16:40:32 -0300

    ✨feat: Finishing sign in screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,19 +1,37 @@
-import * as S from "./styles";
+import { KeyboardAvoidingView, Platform } from "react-native";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+
+import brandImg from "@assets/brand.png";
+
+import * as S from "./styles";
 
 export function SignIn() {
   return (
     <S.Container>
-      <Input
-        placeholder="Email address"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
-      <Input placeholder="Password" type="secondary" secureTextEntry />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <S.Content>
+          <S.Brand source={brandImg} />
 
-      <Button title="Entrar" type="secondary" />
+          <S.Title>Login</S.Title>
+          <Input
+            placeholder="Email address"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input placeholder="Password" type="secondary" secureTextEntry />
+
+          <S.ForgotPasswordButton>
+            <S.ForgotPasswordLabel>Esqueci minha senha</S.ForgotPasswordLabel>
+          </S.ForgotPasswordButton>
+
+          <Button title="Entrar" type="secondary" />
+        </S.Content>
+      </KeyboardAvoidingView>
     </S.Container>
   );
 }
