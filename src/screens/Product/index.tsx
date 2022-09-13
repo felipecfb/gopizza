6570611c375d4328f24d6a434a7a72ad5262606c
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-09-13 14:40:02 -0300

    ✨ feat: Creating component Photo

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import { Platform, TouchableOpacity } from 'react-native';
+import React from "react";
+import { Platform, TouchableOpacity } from "react-native";
 
-import { ButtonBack } from '@components/ButtonBack';
+import { ButtonBack } from "@components/ButtonBack";
+import { Photo } from "@components/Photo";
 
-import * as S from './styles';
+import * as S from "./styles";
 
 export function Product() {
   return (
     <S.Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <S.Header>
         <ButtonBack />
-        <S.Title>
-          Cadastrar
-        </S.Title>
+        <S.Title>Cadastrar</S.Title>
         <TouchableOpacity>
           <S.DeleteLabel>Deletar</S.DeleteLabel>
         </TouchableOpacity>
       </S.Header>
+
+      <Photo uri="" />
     </S.Container>
   );
 }
