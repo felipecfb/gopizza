b97ae69552cc19386a453f17a773062fcba63886
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-09-16 11:46:02 -0300

    ✨ feat: Consuming registered pizzas on firestore

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { TouchableOpacity } from "react-native";
+import React, { useEffect } from "react";
+import { Alert, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import firestore from "@react-native-firebase/firestore";
 
 import happyEmoji from "@assets/happy.png";
 
@@ -8,10 +9,38 @@ import { Search } from "@components/Search";
 
 import { useTheme } from "styled-components/native";
 import * as S from "./styles";
-import { ProductCard } from "@components/ProductCard";
+import { ProductCard, ProductProps } from "@components/ProductCard";
 
 export function Home() {
   const { COLORS } = useTheme();
+
+  function fetchPizzas(value: string) {
+    const formattedValue = value.toLowerCase().trim();
+
+    firestore()
+      .collection("pizzas")
+      .orderBy("name_insensitive")
+      .startAt(formattedValue)
+      .endAt(`${formattedValue}\uf8ff`)
+      .get()
+      .then((response) => {
+        const data = response.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+
+        console.log(data);
+      })
+      .catch(() =>
+        Alert.alert("Consulta", "Não foi possível realizar a consulta")
+      );
+  }
+
+  useEffect(() => {
+    fetchPizzas("");
+  }, []);
 
   return (
     <S.Container>
