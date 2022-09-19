bd503cdcb9345f9aff3e258ddb0210eb07c71f7a
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-09-19 17:26:01 -0300

    ✨ feat: Creating header of screen orders

--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -5,7 +5,9 @@ import * as S from './styles';
 export function Orders() {
   return (
     <S.Container>
-      
+      <S.Header>
+        <S.Title>Pedidos feitos</S.Title>
+      </S.Header>
     </S.Container>
   );
 }
