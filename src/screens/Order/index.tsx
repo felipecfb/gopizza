6debec5a88124973479e59c250a601725e2552e0
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-09-19 16:32:06 -0300

    ✨feat: Listing sizes of product

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Platform } from "react-native";
 
 import { ButtonBack } from "@components/ButtonBack";
 import { RadioButton } from "@components/RadioButton";
 
+import { PIZZA_TYPES } from "@utils/pizzaTypes";
+
 import * as S from "./styles";
 
 export function Order() {
+  const [size, setSize] = useState("");
+
   return (
     <S.Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <S.Header>
@@ -16,9 +20,14 @@ export function Order() {
       <S.Photo source={{ uri: "http://github.com/felipecfb.png" }} />
 
       <S.Sizes>
-        <RadioButton title="Pequeno" selected={false} />
-        <RadioButton title="Médio" selected={true} />
-        <RadioButton title="Grande" selected={false} />
+        {PIZZA_TYPES.map((item) => (
+          <RadioButton
+            key={item.id}
+            title={item.name}
+            onPress={() => setSize(item.id)}
+            selected={size === item.id}
+          />
+        ))}
       </S.Sizes>
     </S.Container>
   );
