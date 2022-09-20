3199031742d71dd9ce0d4947075677c8d9f1dba8
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-09-20 09:05:40 -0300

    ✨feat: Finishing orders screen

--- src/components/OrderCard/index.tsx
+++ src/components/OrderCard/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { TouchableOpacityProps } from 'react-native';
 
 import * as S from './styles';
 
-export function OrderCard() {
-  return (
-    <S.Container>
+type Props = TouchableOpacityProps & {
+  index: number;
+}
 
+export function OrderCard({ index, ...rest }: Props) {
+  return (
+    <S.Container index={index} {...rest}>
+      <S.Image source={{ uri: "https://github.com/felipecfb.png" }} />
+
+      <S.Description>
+        Mesa 5 🞄 Qnt: 1
+      </S.Description>
+
+      <S.StatusContainer status="Preparando">
+        <S.StatusLabel status="Preparando">
+          Preparando
+        </S.StatusLabel>
+      </S.StatusContainer>
     </S.Container>
   );
 }
--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import { OrderCard } from "@components/OrderCard";
+import { ItemSeparator } from "@components/ItemSeparator";
+
 import * as S from './styles';
+import { FlatList } from 'react-native-gesture-handler';
 
 export function Orders() {
   return (
@@ -8,6 +12,19 @@ export function Orders() {
       <S.Header>
         <S.Title>Pedidos feitos</S.Title>
       </S.Header>
+
+      <FlatList
+        data={["1", "2", "3"]}
+        keyExtractor={item => item}
+        renderItem={({ item, index }) => (
+          <OrderCard index={index} />
+        )}
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 24, paddingBottom: 125 }}
+        ItemSeparatorComponent={() => <ItemSeparator />}
+        
+      />
     </S.Container>
   );
 }
