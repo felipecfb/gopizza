c296b2aa6fe9efdaae479cf181b50f4ca1ed2a55
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-09-20 17:52:29 -0300

    ✨ feat: Loading product data on order screen

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,23 +1,51 @@
-import React, { useState } from "react";
-import { Platform } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { Alert, Platform } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import firestore from "@react-native-firebase/firestore";
 
 import { ButtonBack } from "@components/ButtonBack";
 import { RadioButton } from "@components/RadioButton";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { ProductProps } from "@components/ProductCard";
 
 import { PIZZA_TYPES } from "@utils/pizzaTypes";
 
+import { OrderNavigationProps } from "@src/@types/navigation";
+
 import * as S from "./styles";
 
+type PizzaResponse = ProductProps & {
+  price_sizes: {
+    [key: string]: number;
+  };
+};
+
 export function Order() {
-  const navigation = useNavigation();
+  const [pizza, setPizza] = useState<PizzaResponse>({} as PizzaResponse);
   const [size, setSize] = useState("");
+
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const { id } = route.params as OrderNavigationProps;
 
   function handleGoBack() {
     navigation.goBack();
   }
+
+  useEffect(() => {
+    if (id) {
+      firestore()
+        .collection("pizzas")
+        .doc(id)
+        .get()
+        .then((response) => setPizza(response.data() as PizzaResponse))
+        .catch(() =>
+          Alert.alert("Pedido", "Não foi possível carregar o produto")
+        );
+    }
+  }, []);
 
   return (
     <S.Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
@@ -26,10 +54,10 @@ export function Order() {
           <ButtonBack onPress={handleGoBack} style={{ marginBottom: 108 }} />
         </S.Header>
 
-        <S.Photo source={{ uri: "http://github.com/felipecfb.png" }} />
+        <S.Photo source={{ uri: pizza.photo_url }} />
 
         <S.Form>
-          <S.Title>Nome da pizza</S.Title>
+          <S.Title>{pizza.name}</S.Title>
           <S.Label>Selecione um tamanho</S.Label>
           <S.Sizes>
             {PIZZA_TYPES.map((item) => (
